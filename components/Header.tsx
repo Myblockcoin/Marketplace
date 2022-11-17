bd7768a17fe3de4d11e8c14f05bbc836a56af9ff
Ryan Cwynar <ryan@fueledonbacon.com>
2022-11-17 12:34:40 -0500

    format header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,15 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-export default function Header(){
-  return (
-    <div className="container mx-auto py-2">
-    <div className='flex justify-between items-center'>
-      <h1 className='text-xl font-bold'>Blockcoin Marketplace</h1>
-      <div className='flex justify-center'>
-        <ConnectButton />
-      </div>
+export default function Header() {
+    return (
+        <div className="container mx-auto py-2">
+            <div className='flex justify-between items-center'>
+                <h1 className='text-xl font-bold'>Blockcoin Marketplace</h1>
+                <div className='flex justify-center'>
+                    <ConnectButton />
+                </div>
 
-    </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
